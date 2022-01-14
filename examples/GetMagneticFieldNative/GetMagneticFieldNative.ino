d9378e74957aeec4072337e6b3ba27d914475438
Main Push Robot <robot@example.com>
2022-01-14 09:40:29 +0000

    Committing dev changes

--- examples/GetMagneticFieldNative/GetMagneticFieldNative.ino
+++ examples/GetMagneticFieldNative/GetMagneticFieldNative.ino
@@ -12,8 +12,8 @@
 
 #include "Analog-Hall-effect-sensor-Soldered.h"
 
-// Declare the sensor object for analog use with easyC
-Analog_Hall_Sensor hallE;
+// Declare the sensor object for analog use without easyC
+Analog_Hall_Sensor hall(A0); //Parameter is pin on which is connected devices analog pin
 
 void setup()
 {
@@ -21,18 +21,15 @@ void setup()
     Serial.begin(115200);
 
     // Initialize the sensor
-    hallE.begin();
+    hall.begin();
 }
 
 void loop()
 {
-  if(hallE.Available())
-  {
-    Serial.print("Raw value of hall effect sensor with easyC: "); //Print information message
-    Serial.println(hallE.getValue()); //Prints raw value of slider potentiometer
-    Serial.print("Hall effect sensor with easyC reading: "); //Print information message
-    Serial.print(hallE.getMiliTeslas()); //Prints value of hall sensor with easyC in militeslas
-    Serial.println(" mT"); 
-  } 
+  Serial.print("Raw value of hall effect sensor: "); //Print information message
+  Serial.println(hall.getValue()); //Prints raw value of slider potentiometer
+  Serial.print("Hall effect sensor reading: "); //Print information message
+  Serial.print(hall.getMiliTeslas()); //Prints value of hall sensor without easyC in militeslas
+  Serial.println(" mT"); 
   delay(1000);
 }
