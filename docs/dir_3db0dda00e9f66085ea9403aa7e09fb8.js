d40483690e48c0c6562d77b3c1ce9ad52c04437e
Docs Robot <robot@example.com>
2022-02-17 07:04:35 +0000

    Committing documentation changes

---
+++ docs/dir_3db0dda00e9f66085ea9403aa7e09fb8.js
@@ -0,0 +1,7 @@
+var dir_3db0dda00e9f66085ea9403aa7e09fb8 =
+[
+    [ "easyC.cpp", "easy_c_8cpp.html", null ],
+    [ "easyC.h", "easy_c_8h.html", [
+      [ "ANALOG_HALL_EFFECT_SENSOR_EASYC::EasyC", "class_a_n_a_l_o_g___h_a_l_l___e_f_f_e_c_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html", "class_a_n_a_l_o_g___h_a_l_l___e_f_f_e_c_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c" ]
+    ] ]
+];
--- docs/files_dup.js
+++ docs/files_dup.js
@@ -1,11 +1,4 @@
 var files_dup =
 [
-    [ "Analog-Hall-effect-sensor-Soldered.cpp", "_analog-_hall-effect-sensor-_soldered_8cpp.html", null ],
-    [ "Analog-Hall-effect-sensor-Soldered.h", "_analog-_hall-effect-sensor-_soldered_8h.html", [
-      [ "Analog_Hall_Sensor", "class_analog___hall___sensor.html", "class_analog___hall___sensor" ]
-    ] ],
-    [ "easyC.cpp", "easy_c_8cpp.html", null ],
-    [ "easyC.h", "easy_c_8h.html", [
-      [ "ANALOG_HALL_EFFECT_SENSOR_EASYC::EasyC", "class_a_n_a_l_o_g___h_a_l_l___e_f_f_e_c_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html", "class_a_n_a_l_o_g___h_a_l_l___e_f_f_e_c_t___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c" ]
-    ] ]
+    [ "src", "dir_68267d1309a1af8e8297ef4c3efbcdba.html", "dir_68267d1309a1af8e8297ef4c3efbcdba" ]
 ];
