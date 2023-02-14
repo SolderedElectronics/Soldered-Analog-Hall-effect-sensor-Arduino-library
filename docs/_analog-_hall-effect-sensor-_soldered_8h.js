23eec94cad7b7e97c96a66f2785a8bfde837d5c3
Docs Robot <robot@example.com>
2023-02-14 15:22:06 +0000

    Committing documentation changes

---
+++ docs/_analog-_hall-effect-sensor-_soldered_8h.js
@@ -0,0 +1,4 @@
+var _analog__hall_effect_sensor__soldered_8h =
+[
+    [ "Analog_Hall_Sensor", "class_analog___hall___sensor.html", "class_analog___hall___sensor" ]
+];
--- docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
+++ docs/dir_68267d1309a1af8e8297ef4c3efbcdba.js
@@ -2,7 +2,5 @@ var dir_68267d1309a1af8e8297ef4c3efbcdba =
 [
     [ "libs", "dir_87a455bccb09a92130c26a11a880c98a.html", "dir_87a455bccb09a92130c26a11a880c98a" ],
     [ "Analog-Hall-effect-sensor-Soldered.cpp", "_analog-_hall-effect-sensor-_soldered_8cpp.html", null ],
-    [ "Analog-Hall-effect-sensor-Soldered.h", "_analog-_hall-effect-sensor-_soldered_8h.html", [
-      [ "Analog_Hall_Sensor", "class_analog___hall___sensor.html", "class_analog___hall___sensor" ]
-    ] ]
+    [ "Analog-Hall-effect-sensor-Soldered.h", "_analog-_hall-effect-sensor-_soldered_8h.html", "_analog-_hall-effect-sensor-_soldered_8h" ]
 ];
